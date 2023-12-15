d4dac1b268016b95fa805c3fdc610809431647c5
jeandrei <jeandreiwalter@gmail.com>
2023-12-15 15:55:47 +0000

    testando livechat

--- filaunica/app/views/pages/index.php
+++ filaunica/app/views/pages/index.php
@@ -127,10 +127,24 @@
 </div> 
 
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/657c730707843602b8027299/1hhn22m52';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <a href="https://web.whatsapp.com/send?phone=554733454025" class="float" target="_blank">
 <i class="fa fa-whatsapp my-float"></i>
-</a>
+</a> -->
 
  
 </div><!--fecha div container lá do header-->
